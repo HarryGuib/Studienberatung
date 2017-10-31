d5fabbd3a8421820075a54453aa197a6550cfefa
HarryGuib <selim.ariguib@gmail.com>
2017-10-31 22:38:18 +0100

    1 frage

--- Studienberatung.java
+++ Studienberatung.java
@@ -4,7 +4,9 @@ public class Studienberatung{
 		public static void main(String[] args)
 		{
 			Scanner scanner = new Scanner (System.in);
-
+			System.out.println("Hast du Abitur ?");
+			String antwort = scanner.nextLine();
+			
 		}
 		
 }
